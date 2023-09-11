6645b671636c4d9b0d3f81e2b2d998d48c4cea72
ebrahim-s-ebrahim <ebrahim3adel@gmail.com>
2023-09-11 13:48:42 +0300

    some refactoring

--- Program.cs
+++ Program.cs
@@ -4,36 +4,43 @@ using System.IO;
 using System.IO.Pipes;
 using System.Text.RegularExpressions;
 
-namespace ccwc
+namespace ccwc;
+
+public class Program
 {
-    public class Program
+    static async Task<int> Main(string[] args)
     {
-        static async Task<int> Main(string[] args)
+        var fileArgument = new Argument<FileInfo?>(
+            name: "file",
+            description: "The required file.");
+
+        var countBytesOption = new Option<bool>(
+            name: "--byte",
+            description: "The number of bytes in a file.");
+
+        var countWordsOption = new Option<bool>(
+            name: "--word",
+            description: "The number of words in a file.");
+
+        var countLinesOption = new Option<bool>(
+            name: "--line",
+            description: "The number of lines in a file.");
+
+        var rootCommand = new RootCommand("Word counter command line tool");
+        rootCommand.AddArgument(fileArgument);
+        rootCommand.AddOption(countBytesOption);
+        rootCommand.AddOption(countWordsOption);
+        rootCommand.AddOption(countLinesOption);
+
+        rootCommand.SetHandler(CountHandler, fileArgument, countBytesOption, countWordsOption, countLinesOption);
+
+        if (args.Length > 0)
         {
-            var fileArgument = new Argument<FileInfo?>(
-                name: "file",
-                description: "The required file.");
-
-            var countBytesOption = new Option<bool>(
-                name: "--byte",
-                description: "The number of bytes in a file.");
-
-            var countWordsOption = new Option<bool>(
-                name: "--word",
-                description: "The number of words in a file.");
-
-            var countLinesOption = new Option<bool>(
-                name: "--line",
-                description: "The number of lines in a file.");
-
-            var rootCommand = new RootCommand("Word counter command line tool");
-            rootCommand.AddArgument(fileArgument);
-            rootCommand.AddOption(countBytesOption);
-            rootCommand.AddOption(countWordsOption);
-            rootCommand.AddOption(countLinesOption);
-
-            rootCommand.SetHandler(CountHandler, fileArgument, countBytesOption, countWordsOption, countLinesOption);
-
+            await rootCommand.InvokeAsync(args);
+        }
+        else
+        {
+            // Enter the interactive loop for user input.
             bool exitRequested = false;
             while (!exitRequested)
             {
@@ -45,85 +52,63 @@ namespace ccwc
                 }
                 else
                 {
-                    await rootCommand.InvokeAsync(commandInput);
+                    var inputArgs = commandInput.Split(' ');
+
+                    if (inputArgs.Length < 2 || string.IsNullOrEmpty(inputArgs[1]))
+                    {
+                        Console.WriteLine("Please enter the file name:");
+                        var fileName = Console.ReadLine();
+                        inputArgs = new[] { inputArgs[0], fileName };
+                    }
+
+                    await rootCommand.InvokeAsync(inputArgs);
                 }
             }
-
-            return 0;
         }
 
+        return 0;
+    }
 
-        static void CountHandler(FileInfo file, bool countBytes, bool countWords, bool countLines)
+
+    static void CountHandler(FileInfo file, bool countBytes, bool countWords, bool countLines)
+    {
+        if (file == null)
         {
-            if (file == null)
-            {
-                Console.WriteLine("File not provided");
-                return;
-            }
-
-            if (countBytes)
-            {
-                var bytes = GetBytes(file);
-                Console.WriteLine($"Bytes: {bytes}");
-            }
-
-            if (countWords)
-            {
-                var words = GetWords(file);
-                Console.WriteLine($"Words: {words}");
-            }
-
-            if (countLines)
-            {
-                var lines = GetLines(file);
-                Console.WriteLine($"Lines: {lines}");
-            }
+            Console.WriteLine("File not provided");
+            return;
         }
 
-
-        private static long GetLines(FileInfo file)
+        string fileContents;
+        using (var myfilestream = file.OpenRead())
+        using (var reader = new StreamReader(myfilestream))
         {
-            var myfilestream = file.OpenRead();
-
-            string fileContents;
-            using (StreamReader reader = new StreamReader(myfilestream))
-            {
-                fileContents = reader.ReadToEnd();
-            }
-            var lines = Regex.Matches(fileContents, "\n").Count + 1;
-            return lines;
+            fileContents = reader.ReadToEnd();
         }
 
-        private static long GetWords(FileInfo file)
+        if (countBytes)
         {
-            var myfilestream = file.OpenRead();
+            var bytes = fileContents.Length;
+            Console.WriteLine($"Bytes: {bytes}");
+        }
 
-            string fileContents;
-            using (StreamReader reader = new StreamReader(myfilestream))
-            {
-                fileContents = reader.ReadToEnd();
-            }
+        if (countWords)
+        {
             var words = Regex.Matches(fileContents, @"\b\w+\b").Count;
-            return words;
+            Console.WriteLine($"Words: {words}");
         }
 
-        private static long GetBytes(FileInfo file)
+        if (countLines)
         {
-            long bytes;
-            using (var fileStream = file.OpenRead())
-            {
-                bytes = fileStream.Length;
-            }
-
-            return bytes;
-        }
-
-        static string PromptForCommand()
-        {
-            Console.WriteLine("Enter the command and options (or type 'exit' to quit):");
-            var commandInput = Console.ReadLine();
-            return commandInput;
+            var lines = Regex.Matches(fileContents, "\n").Count + 1;
+            Console.WriteLine($"Lines: {lines}");
         }
     }
 
+    static string PromptForCommand()
+    {
+        Console.WriteLine("Enter the command and options (or type 'exit' to quit):");
+        var commandInput = Console.ReadLine();
+        return commandInput;
+    }
 }
+
